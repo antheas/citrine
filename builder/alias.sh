01de20c1dceaa47ab98aff5c06f024fd5646fd74
antheas <git@antheas.dev>
2024-05-09 22:19:18 +0200

    patch rpm-ostree to skip error

--- builder/alias.sh
+++ builder/alias.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 alias fd="docker run\
-    -it --rm -v $(pwd):/workspace -w /workspace \
+    -it --rm -v \$(pwd):/workspace -w /workspace \
     --privileged --device /dev/fuse --security-opt label:disable \
-    -u $(id -u):$(id -g) \
+    -u \$(id -u):\$(id -g) \
     fedora_build"
